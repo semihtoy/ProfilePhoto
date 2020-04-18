6887a74abaa66473d8de010995eed597a8d455a6
ssemih42 <ssemih4238@gmail.com>
2020-04-18 14:15:29 +0300

    Text edit

---
+++ ProfilePhoto/Model/Links.swift
@@ -0,0 +1,18 @@
+import Foundation
+struct Links : Codable {
+	let selfLink : SelfLink?
+	let edit : Edit?
+
+	enum CodingKeys: String, CodingKey {
+
+		case selfLink = "self"
+		case edit = "edit"
+	}
+
+	init(from decoder: Decoder) throws {
+		let values = try decoder.container(keyedBy: CodingKeys.self)
+		selfLink = try values.decodeIfPresent(SelfLink.self, forKey: .selfLink)
+		edit = try values.decodeIfPresent(Edit.self, forKey: .edit)
+	}
+
+}
